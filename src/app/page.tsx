da064f67c7baad3cb6c9c6997c77365d555f05ab
jongdae <asdfg9377@naver.com>
2025-04-03 00:16:46 +0900

    feat: import global scss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { FaqManager } from '@/examples/FaqExamples';
 
 import { Providers } from './components/providers';
 import styles from './page.module.css';
+import '@/app/styles/globals.scss';
 
 export default function Home() {
   return (
