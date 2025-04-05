14625a57f883b7e671eb7c25774aefd5a158c6fb
jongdae <asdfg9377@naver.com>
2025-04-05 14:34:30 +0900

    feat: icon component

---
+++ src/shared/assets/icons/index.tsx
@@ -0,0 +1,25 @@
+import ArrowDownIcon from './ic_arrow_down.svg';
+import ArrowRightIcon from './ic_arrow_right.svg';
+import ArrowUpIcon from './ic_arrow_up.svg';
+import DownloadIcon from './ic_download.svg';
+import ProcessIcon from './ic_process.svg';
+import Process02Icon from './ic_process02.svg';
+import Process03Icon from './ic_process03.svg';
+import Process04Icon from './ic_process04.svg';
+import SearchIcon from './ic_search.svg';
+import TalkIcon from './ic_talk.svg';
+import WriteIcon from './ic_write.svg';
+
+export {
+  ArrowDownIcon,
+  ArrowRightIcon,
+  ArrowUpIcon,
+  DownloadIcon,
+  ProcessIcon,
+  Process02Icon,
+  Process03Icon,
+  Process04Icon,
+  SearchIcon,
+  TalkIcon,
+  WriteIcon,
+};
