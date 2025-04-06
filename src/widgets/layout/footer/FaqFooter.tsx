34c18a1945e7b8e887380be27e236b53ea070f30
jongdae <asdfg9377@naver.com>
2025-04-06 22:01:15 +0900

    refactor: footer data 상수화

--- src/widgets/layout/footer/FaqFooter.tsx
+++ src/widgets/layout/footer/FaqFooter.tsx
@@ -4,10 +4,16 @@ import { useState } from 'react';
 
 import TermsDialog from '@/entities/terms/ui/TermsDialog';
 
+import { BUSINESS_INFO, COPYRIGHT, POLICY_LINKS, TERMS_DEFAULT_VERSION } from './constants';
 import styles from './FaqFooter.module.scss';
 
 export const FaqFooter: React.FC = () => {
   const [isTermsOpen, setIsTermsOpen] = useState(false);
+
+  const handleTermsClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsTermsOpen(true);
+  };
 
   return (
     <footer className={styles.footer}>
@@ -22,50 +28,52 @@ export const FaqFooter: React.FC = () => {
               height={40}
               className={styles.logo}
             />
-            <p className={styles.copyright}> 2023 KIA CORP. All Rights Reserved.</p>
+            <p className={styles.copyright}>{COPYRIGHT}</p>
           </div>
 
           <div className={styles.contentSection}>
             {/* policy links */}
             <div className={styles.policyLinks}>
               <ul className={styles.linkList}>
-                <li>
-                  <a
-                    href="https://privacy.kia.com/overview/full-policy"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    개인정보 처리방침
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    onClick={e => {
-                      e.preventDefault();
-                      setIsTermsOpen(true);
-                    }}
-                  >
-                    이용 약관
-                  </a>
-                </li>
+                {POLICY_LINKS.map(link => (
+                  <li key={link.id}>
+                    {link.isDialog ? (
+                      <a href={link.url} onClick={handleTermsClick}>
+                        {link.name}
+                      </a>
+                    ) : (
+                      <a
+                        href={link.url}
+                        target={link.isExternal ? '_blank' : undefined}
+                        rel={link.isExternal ? 'noopener noreferrer' : undefined}
+                      >
+                        {link.name}
+                      </a>
+                    )}
+                  </li>
+                ))}
               </ul>
             </div>
 
             {/* business info */}
             <div className={styles.businessInfoWrapper}>
               <ul className={`${styles.linkList} ${styles.businessInfo}`}>
-                <li>서울특별시 서초구 헌릉로 12 기아㈜</li>
-                <li>대표: 송호성, 최준영</li>
-                <li>사업자등록번호: 119-81-02316</li>
-                <li>통신판매번호: 2006-07935</li>
-                <li>고객센터: 1833-4964</li>
-                <li>
-                  제휴문의:{' '}
-                  <a href="mailto:kiabiz@kia.com" className={styles.emailLink}>
-                    kiabiz@kia.com
-                  </a>
-                </li>
+                {BUSINESS_INFO.map(item => (
+                  <li key={item.id}>
+                    {item.text}
+                    {item.link && (
+                      <>
+                        {' '}
+                        <a
+                          href={item.link.url}
+                          className={item.link.className ? styles[item.link.className] : undefined}
+                        >
+                          {item.link.text}
+                        </a>
+                      </>
+                    )}
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
@@ -76,7 +84,7 @@ export const FaqFooter: React.FC = () => {
       <TermsDialog
         isOpen={isTermsOpen}
         onClose={() => setIsTermsOpen(false)}
-        initialVersion={5} // 최신 버전(5)을 기본값으로 설정
+        initialVersion={TERMS_DEFAULT_VERSION}
       />
     </footer>
   );
